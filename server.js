8ec5ee26c9f7294bc027d6fc3f3094d8e4271293
Kapil1721 <107835784+Kapil1721@users.noreply.github.com>
2023-08-29 18:27:58 +0530

    asdsad

--- server.js
+++ server.js
@@ -15,7 +15,11 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD
 );
 
-mongoose.connect(DB).then(() => console.log("DB connection successful!"));
+mongoose
+  .connect(
+    "mongodb+srv://sahilEgss05:cEN2JXfKnB8OIZnk@cluster0.fku9vqp.mongodb.net/review-website"
+  )
+  .then(() => console.log("DB connection successful!"));
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
