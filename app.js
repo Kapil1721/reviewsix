2d9290aef1e6ab912b1921008ca3cdc448acd2a8
Kapil1721 <107835784+Kapil1721@users.noreply.github.com>
2023-08-31 10:34:37 +0530

    asdsad

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express = require("express");
 const helmet = require("helmet");
 const dotenv = require("dotenv");
 
+dotenv.config({ path: "./config.env" });
+
+const mongoose = require("mongoose");
+
 const globalErrorHandler = require("./controllers/errorController");
 
 // -------------------------------------------------
@@ -18,7 +22,21 @@ const AppError = require("./utils/appError");
 
 // *-------------------------------------
 
+const port = process.env.PORT || 3000;
+
 const app = express();
+
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXCEPTION! 💥 Shutting down...");
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://sahilEgss05:cEN2JXfKnB8OIZnk@cluster0.fku9vqp.mongodb.net/review-website"
+  )
+  .then(() => console.log("DB connection successful!"));
 
 // * ---------------------------
 dotenv.config({ path: "./config.env" });
@@ -46,4 +64,19 @@ app.all("*", (req, res, next) => {
 
 app.use(globalErrorHandler);
 
-module.exports = app;
+// const DB = process.env.DATABASE.replace(
+//   "<PASSWORD>",
+//   process.env.DATABASE_PASSWORD
+// );
+
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED REJECTION! 💥 Shutting down...");
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
